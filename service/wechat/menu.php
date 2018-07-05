e70f8e054be320fc91add9d84f0f31b61e57752a
Yungno <Yungno@yungno-MacBook-Pro.local>
2018-07-05 22:55:25 +0800

    menu.php修改

--- service/wechat/menu.php
+++ service/wechat/menu.php
@@ -1,5 +1,17 @@
 <?php
-  // 这个只是样例哦，还要改
+/*
+|--------------------------------------------------------------------------
+| 公众号底部菜单栏
+|--------------------------------------------------------------------------
+|
+|一周一故事下三个按钮
+|最新：从数据库中选择4个最新的可用故事
+|合作&bug反馈：url跳转
+|
+|
+|
+|
+*/
   $appid = 'wx5ac7dbb5d6163772';
   $secret = '6092e58e0eed5abeae329596b6b866f9';
   $token = 'zuggrcampus';
@@ -27,3 +39,23 @@
   ]];
   $wx = new angel\wechat($GLOBALS['wechat_config']['appid'], $GLOBALS['wechat_config']['secret'], $GLOBALS['wechat_config']['token']);
   echo $wx->menu($wx->access_token(), $menu);
+  $wx->listen('event','CLICK',function($input,$wx){
+      switch($input->EventKey){
+        case 'story_more':
+          $stories = sql::select('story')->where('activate=1')->order('id')->by('desc')->limit(4)->fetch();
+          $out = [];
+          foreach($stories as $story){
+            $out[] = [
+              'title' => $story['title'],
+              'description' => str::utf8($story['description']),
+              'picurl' => user::url().'/file/img/'.$story['headimg'],
+              'url' => user::url().'/tran/wechat/'.$input->FromUserName.'/story+'.$story['id']//看一下这行是否有问题
+            ];
+          }
+          $wx->return('news',[
+            'to' => $input->FromUserName,
+            'articles' => $out
+          ]); //返回主题
+        break;
+      }
+    }); //监听点击
