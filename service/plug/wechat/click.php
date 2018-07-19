37a59eb32f832bc5368ded0347092c459f85a690
yongnuo <yangyn2002@outlook.com>
2018-07-19 23:48:31 +0800

    增加menu “最新主题”

--- service/plug/wechat/click.php
+++ service/plug/wechat/click.php
@@ -13,16 +13,23 @@
 */
 
   switch ($input->EventKey) {
-    case 't':
+    case 'story_more':
+    $stories = sql::select('story')->where('activate=1')->order('id')->by('desc')->limit(4)->fetch();
+    $out = [];
+    foreach($stories as $story){
+      $out[] = [
+        'title' => $story['title'],
+        'description' => str::utf8($story['description']),
+        'picurl' => user::url().'/file/img/'.$story['headimg'],
+        'url' => user::url().'/tran/wechat/'.$input->FromUserName.'/story+'.$story['id']
+          ];
+              }
     $wx->return('news',[
-       'to' => $input->FromUserName,
-       'articles' => [[
-         'title' => '订阅成功！',
-         'description' => 'abc',
-         'picurl' => '',
-         'url' => 'https://baidu.com'
-       ]]
-     ]);
+                'to' => $input->FromUserName,
+                'articles' => $out
+              ]); //返回主题
+    break;
+
     break;
     case 'event_b':
       // 样板2
